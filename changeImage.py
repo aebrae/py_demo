f8dbc34824fdf25d0b761bf3ef87d392da7a0e14
Alex Ebrahim <75652016+aebrae@users.noreply.github.com>
2023-10-04 14:53:18 -0700

    Create changeImage.py

---
+++ changeImage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Process the supplier images to the following specs: 
+        --change resolution from 3000x2000 to 600x400 pixel
+        --change image format from .TIFF to .JPEG
+"""
+import os
+from PIL import Image
+
+source_path      = '/home/supplier_data/images/'
+destination_path = '/home/supplier_data/images/'
+
+for file in os.listdir(source_path):
+        if file.endswith('tiff'):
+                im = Image.open(source_path+file)
+                #print(destination_path+file[:-4]+"jpeg")
+                new_im = im.convert("RGB").resize((600,400)).save(destination_path+file[:-4]+"jpeg",format="jpeg") 
